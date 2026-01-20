4aad2fb8a2c8596f4cf0793571716fc4ae16d221
Creeper343 <72136874+Creeper343@users.noreply.github.com>
2026-01-20 10:40:06 +0100

    Nick code

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Warnung: Dies erlaubt den Build, auch wenn ESLint-Fehler vorhanden sind.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Warnung: Dies erlaubt den Build, auch wenn TypeScript-Fehler vorhanden sind.
     ignoreBuildErrors: true,
